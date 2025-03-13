cfd83e6e3fdaef4f66459b0dfa4d190d4b47be3c
KimSia, Sim <245021+simkimsia@users.noreply.github.com>
2025-03-13 12:05:41 +0700

    fix: improve the suggestions.py
    Signed-off-by: KimSia, Sim <245021+simkimsia@users.noreply.github.com>

--- .dagger/__init__.py
+++ .dagger/__init__.py
@@ -1,6 +1,5 @@
 from .diff_parser import Diff, DiffParser, FileChange, LineChange
 from .github_client import GithubClient
-from .suggestions import parse_diff
 
 __all__ = [
     "DiffParser",
@@ -8,5 +7,4 @@ __all__ = [
     "FileChange",
     "LineChange",
     "GithubClient",
-    "parse_diff",
 ]
